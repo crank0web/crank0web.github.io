168d5857a6bd214919f2c78689981fe3c63ba96c
hub8905 <165077605+hub8905@users.noreply.github.com>
2024-03-27 09:21:35 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -1,49 +1,49 @@
 new class{
-    siteurl="https://api.c-rank.online"
-    xscript=/<script>(.*?)<\/script>/g
-    #aside
-    #main
-    set aside(html){
-        var script=[]
-        this.#aside.innerHTML=html.replace(xscript,(_,src)=>script.push(src))
-        for(var src of script){
-            Function(src).call(this)
-        }
-    }
-    set main(html){
-        var script=[]
-        this.#main.innerHTML=html.replace(xscript,(_,src)=>script.push(src))
-        for(var src of script){
-            Function(src).call(this)
-        }
-    }
-    constructor(){
-        this.init()
-    }
-    async init(){
-        var {code}=await this.xget("/signon")
-        if(code==200){
-            this.aside=await this.xget("/aside")
-            this.main=await this.get("/page/home.htm")
-        }else{
-            this.aside=""
-            this.main=await this.get("/page/signin.htm")
-        }
-    }
-    async get(url){
-        var res=await fetch(url)
-        var text=await res.text()
-        try{
-            return JSON.parse(text)
-        }catch(e){}
-        return text
-    }
-    async xget(url){
-        var res=await fetch(this.siteurl+url)
-        var text=await res.text()
-        try{
-            return JSON.parse(text)
-        }catch(e){}
-        return text
-    }
+	siteurl="https://api.c-rank.online"
+	xscript=/<script>(.*?)<\/script>/g
+	#aside
+	#main
+	set aside(html){
+		var script=[]
+		this.#aside.innerHTML=html.replace(xscript,(_,src)=>script.push(src))
+		for(var src of script){
+			Function(src).call(this)
+		}
+	}
+	set main(html){
+		var script=[]
+		this.#main.innerHTML=html.replace(xscript,(_,src)=>script.push(src))
+		for(var src of script){
+			Function(src).call(this)
+		}
+	}
+	constructor(){
+		this.init()
+	}
+	async init(){
+		var {code}=await this.xget("/signon")
+		if(code==200){
+			this.aside=await this.xget("/aside")
+			this.main=await this.get("/page/home.htm")
+		}else{
+			this.aside=""
+			this.main=await this.get("/page/signin.htm")
+		}
+	}
+	async get(url){
+		var res=await fetch(url)
+		var text=await res.text()
+		try{
+			return JSON.parse(text)
+		}catch(e){}
+		return text
+	}
+	async xget(url){
+		var res=await fetch(this.siteurl+url)
+		var text=await res.text()
+		try{
+			return JSON.parse(text)
+		}catch(e){}
+		return text
+	}
 }
